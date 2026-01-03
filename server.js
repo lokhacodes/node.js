d671e85921431460100fdda0ade7f9b56c1ff820
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-03 21:42:45 +0600

    add path parameter

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 import http from 'node:http'
 import {getDataFromDB} from './db.js'
+
 const PORT = 8000
 
 /*const animal = {
@@ -19,12 +20,25 @@ const server = http.createServer(async(req, res) =>{
           res.setHeader ('Content-Type', 'application/json')
           res.statusCode = 200
           res.end(JSON.stringify(destinations))
-     }else {
+     }else if (req.url.startsWith('/api/continent') && req.method ==='GET'){
+          const continent = req.url.split('/').pop()
+          const filteredData = destinations.filter((destination) => {
+               return destination.continent.toLowerCase() === continent.toLowerCase()
+          })
+
+          res.setHeader ('Content-Type', 'application/json')
+          res.statusCode = 200
+          res.end(JSON.stringify(filteredData))
+     }
+
+     else {
           res.setHeader ('Content-Type', 'application/json')
           res.statusCode = 404
-          res.end (JSON.stringify({message: 'Route not found'}))
+          res.end (JSON.stringify({
+               error : "not found",
+               message : "Route not found"}))
      }
     // res.end('hello from the server!', ()=>console.log('response end'))
 })
 
-server.listen (PORT, () => console.log(`server running on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))

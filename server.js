766acfaae70bc4e3cb71d6775896c42c9343f6c6
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-03 20:29:29 +0600

    error handling

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 
 import http from 'node:http'
+import {getDataFromDB} from './db.js'
 const PORT = 8000
 
-const server = http.createServer((req, res) =>{
-     res.write(' this is some data \n')
-     
+/*const animal = {
+     type: 'elephant',
+     nickName: 'Elon Tusk'
+}
+
+console.log(JSON.stringify(animal)) */
+
+
+const server = http.createServer(async(req, res) =>{
+     const destinations = await getDataFromDB()
+   //  res.write(' this is some data \n')
+
      if(req.url === '/api' && req.method ==='GET'){
-          res.end('this is form the server...')
+          res.setHeader ('Content-Type', 'application/json')
+          res.statusCode = 200
+          res.end(JSON.stringify(destinations))
+     }else {
+          res.setHeader ('Content-Type', 'application/json')
+          res.statusCode = 404
+          res.end (JSON.stringify({message: 'Route not found'}))
      }
-     res.end('hello from the server!', ()=>console.log('response end'))
+    // res.end('hello from the server!', ()=>console.log('response end'))
 })
 
 server.listen (PORT, () => console.log(`server running on port: ${PORT}`))

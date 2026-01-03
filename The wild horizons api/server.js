d0828dda1d5a77484d42b342d34edff63331f8af
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-03 22:31:36 +0600

    Query params

--- The wild horizons api/server.js
+++ The wild horizons api/server.js
@@ -18,8 +18,19 @@ const server = http.createServer(async(req, res) =>{
      const destinations = await getDataFromDB()
    //  res.write(' this is some data \n')
 
-     if(req.url === '/api' && req.method ==='GET'){
-         sendJSONResponse(res, 200, destinations)
+
+     const urlObj = new URL(req.url, `http://${req.headers.host}`)
+     const queryObj = Object.fromEntries(urlObj.searchParams)
+
+
+
+
+
+
+
+     if(urlObj.pathname === '/api' && req.method ==='GET'){
+          let filteredDestinations = destinations
+         sendJSONResponse(res, 200, filteredDestinations)
      }else if (req.url.startsWith('/api/continent') && req.method ==='GET'){
           const continent = req.url.split('/').pop()
           const filteredData = getDataByParams(destinations, 'continent', continent)

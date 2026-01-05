467df69e1655f56d9feb94e0f093fee97ea77ea1
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-05 23:55:22 +0600

    server folder

--- NODE app/server.js
+++ NODE app/server.js
@@ -2,6 +2,7 @@
 import http from 'node:http'
 import path from 'node:path'
 import { serverStatic } from './utils/serverStatic.js'
+import fs from  'node:fs'
 
 
 const PORT = 8000
@@ -10,13 +11,14 @@ const __dirname = import.meta.dirname
 
 // node "NODE app/server.js"
 
-const server = http.createServer(async(req, res) => {
+const server = http.createServer((req, res) => {
    //const pathToResource = path.join(__dirname, 'public', 'index.html');//absolute path
    serverStatic(__dirname); 
+   const content = fs.readFileSync(pathToResource, 'utf-8')
    res.setHeader('Content-Type', 'text/html');
    res.statusCode = 200;
    // res.writeHead(200,{'Content-Type':'text/html'});
-    res.end();
+    res.end(content);
 })
 
 server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))
--- NODE app/utils/serverStatic.js
+++ NODE app/utils/serverStatic.js
@@ -1,5 +1,16 @@
 import path from 'node:path';
-export function serverStatic(baseDir) {
+import fs from 'node:fs';
+
+export function serverStatic(baseDir, req, res) {
     const filePath = path.join(baseDir, 'public', 'index.html');
     console.log(filePath);
+    if (fs.existsSync(filePath)) {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.end(content);
+    } else {
+        res.statusCode = 404;
+        res.end('File not found');
+    }
 }

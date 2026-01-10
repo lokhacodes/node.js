b81a924e581d8d1270c72e3c461bab107c802e20
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-10 23:50:08 +0600

    serve index.html

--- NODE app/utils/serverStatic.js
+++ NODE app/utils/serverStatic.js
@@ -1,16 +1,12 @@
 import path from 'node:path';
 import fs from 'node:fs';
-
-export function serverStatic(baseDir) {
+import { sendResponse } from './sendResponse.js';
+export async function serverStatic(baseDir) {
     const filePath = path.join(baseDir, 'public', 'index.html');
-    console.log(filePath);
-    if (fs.existsSync(filePath)) {
-        const content = fs.readFileSync(filePath, 'utf-8');
-        res.setHeader('Content-Type', 'text/html');
-        res.statusCode = 200;
-        res.end(content);
-    } else {
-        res.statusCode = 404;
-        res.end('File not found');
+    try {
+        const content = await fs.readFile(filePath)
+        sendResponse(res, 200, 'text/html', content)
+    } catch (err) {
+        console.log(err);
     }
-}
+     }

ebed540dd8d0e83301e71ea9d61cae5725351248
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-11 23:59:33 +0600

    serve frontend

--- NODE app/server.js
+++ NODE app/server.js
@@ -1,38 +1,12 @@
-
 import http from 'node:http'
-import path from 'node:path'
-//import { serverStatic } from './utils/serverStatic.js'
-import fs from  'node:fs/promises'
-
+import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
+
 const __dirname = import.meta.dirname
 
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname)
+}) 
 
-// node "NODE app/server.js"
-
-const server = http.createServer(async(req, res) => {
-   /*const pathToResource = path.join(__dirname, 'public');//absolute path
-  // serverStatic(__dirname); 
-   //const content = fs.readFileSync(pathToResource, 'utf-8')
-  // fs.readFile(pathToResource, 'utf-8',(err,content)=>{
-        //if(err){
-                //console.log(err);
-               // return
-            //}*/
-   const publicDir = path.join(__dirname, 'public')
-    const pathToResource = path.join(
-       publicDir,
-       req.url === '/' ? 'index.html' : req.url
-  )
-
-        const content = await fs.readFile(pathToResource)
-        const ext = path.extname(pathToResource);
-        res.setHeader('Content-Type', 'text/html');
-        res.statusCode = 200;
-        // res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(content);
-   // })
-
-})
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
--- NODE app/utils/serverStatic.js
+++ NODE app/utils/serverStatic.js
@@ -1,12 +1,22 @@
 import path from 'node:path';
 import fs from 'node:fs';
 import { sendResponse } from './sendResponse.js';
-export async function serverStatic(baseDir) {
-    const filePath = path.join(baseDir, 'public', 'index.html');
-    try {
+import { getContentType } from './utils/getaContentType'
+export async function serverStatic(req, res, baseDir) {
+   
+    const publicDir = path.join(baseDir, 'public')
+        const filePath = path.join(
+        publicDir,
+        req.url === '/' ? 'index.html' : req.url
+        )
+
+        const ext = path.extname(filePath)
+        const contentType = getContentType(ext)
+
+        try {
         const content = await fs.readFile(filePath)
-        sendResponse(res, 200, 'text/html', content)
-    } catch (err) {
-        console.log(err);
-    }
-     }
+        sendResponse(res, 200, contentType, content)
+        } catch (err) {
+        console.log(err)
+        }
+}

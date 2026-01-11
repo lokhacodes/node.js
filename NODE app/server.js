8daa313eada1e30bbfc47ab00f452018d1a8d618
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-11 23:44:00 +0600

    Aside serve multiple asset

--- NODE app/server.js
+++ NODE app/server.js
@@ -12,15 +12,22 @@ const __dirname = import.meta.dirname
 // node "NODE app/server.js"
 
 const server = http.createServer(async(req, res) => {
-   const pathToResource = path.join(__dirname, 'public', 'index.html');//absolute path
+   /*const pathToResource = path.join(__dirname, 'public');//absolute path
   // serverStatic(__dirname); 
    //const content = fs.readFileSync(pathToResource, 'utf-8')
   // fs.readFile(pathToResource, 'utf-8',(err,content)=>{
         //if(err){
                 //console.log(err);
                // return
-            //}
-    const content = await fs.readFile(pathToResource, 'utf-8')
+            //}*/
+   const publicDir = path.join(__dirname, 'public')
+    const pathToResource = path.join(
+       publicDir,
+       req.url === '/' ? 'index.html' : req.url
+  )
+
+        const content = await fs.readFile(pathToResource)
+        const ext = path.extname(pathToResource);
         res.setHeader('Content-Type', 'text/html');
         res.statusCode = 200;
         // res.writeHead(200,{'Content-Type':'text/html'});
